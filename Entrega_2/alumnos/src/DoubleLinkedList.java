fe0187d7143a8820f86b6a60f1fbe7ff7942e369
plopez053 <peiolpz02@gmail.com>
2023-11-04 18:51:45 +0100

    UnorderedLinkedList no se si va

--- Entrega_2/alumnos/src/DoubleLinkedList.java
+++ Entrega_2/alumnos/src/DoubleLinkedList.java
@@ -1,4 +1,4 @@
-package segundaFase;
+package Entregas.segundaFase;
 
 import java.util.Iterator;
 import java.util.NoSuchElementException;
@@ -24,7 +24,7 @@ public class DoubleLinkedList<T> implements ListADT<T> {
 	public String getDescr() {
 	  return descr;
 	}
-//
+
 	public T removeFirst() {
 	// Elimina el primer elemento de la lista
         // Precondici�n: 
@@ -32,37 +32,83 @@ public class DoubleLinkedList<T> implements ListADT<T> {
 		if(isEmpty()) {
 			System.out.println("La lista esta vacia");
 		}else if(this.size() == 1) {
+			Node<T> devolver = this.first;
 			this.first = null;
 			this.count -= 1;
+			return devolver.data;
 		}else {
 			Node<T> actual = this.first;
 			this.first = this.first.next;
 			this.first.prev = actual.prev;
 			this.first.next = actual.next.next;
 			this.count -= 1;
+			return actual.data;
 		}
+		return null;
 	}
 	
 	public T removeLast() {
 	// Elimina el �ltimo elemento de la lista
         // Precondici�n: 
 		// COMPLETAR EL CODIGO Y CALCULAR EL COSTE
-		if(isEmpty()) {
-			System.out.println("La lista esta vacia");
-		}
+		if (isEmpty()) {
+			System.out.println("La lista está vacía");
+		    return null;
+		} else if (this.size() == 1) {
+	        Node<T> devolver = this.first;
+	        this.first = null;
+		    this.count -= 1;
+		    return devolver.data;
+	    } else {
+	        Node<T> actual = this.first;
+	        while (actual.next != this.first) {
+	        	actual = actual.next;
+		    }
+		    Node<T> devolver = actual;
+		    actual.prev.next = this.first;
+		    this.first.prev = actual.prev;
+	        this.count--;
+	        return devolver.data;
+	    }
    }
 
 
 	public T remove(T elem) {
 	//Elimina un elemento concreto de la lista
 		// COMPLETAR EL CODIGO Y CALCULAR EL COSTE
+		if (isEmpty()) {
+	        System.out.println("La lista está vacía");
+	        return null;
+	    } else {
+	        Node<T> actual = this.first;
+	        Node<T> nodoAnterior = null;
+	        do {
+	        	if (actual.data.equals(elem)) {
+	        		if (this.size() == 1) {
+	        			this.first = null;
+		            } else {
+		            	if (actual == this.first) {
+		            		this.first = actual.next;
+		                }
+		            	nodoAnterior.next = actual.next;
+		            	actual.next.prev = nodoAnterior;
+		            }
+	        		this.count--;
+	        		return actual.data;
+	        	}
+	        	nodoAnterior = actual;
+		        actual = actual.next;
+	        } while (actual != this.first);
+	    }
+		System.out.println("El elemento no se encuentra en la lista");
+		return null;
 	}
 
 	public T first() {
 	//Da acceso al primer elemento de la lista
 	      if (isEmpty())
 	          return null;
-	      else return this.first;
+	      else return this.first.data;
 	}
 
 	public T last() {
@@ -74,13 +120,34 @@ public class DoubleLinkedList<T> implements ListADT<T> {
 
 	public boolean contains(T elem) {
 	//Determina si la lista contiene un elemento concreto
-		      if (isEmpty())
-		          return false;
+		boolean enc = false;
+		if (isEmpty()) {
+			enc = false;
+		}else {
+			Node<T> actual = this.first;
+			while(this.first.prev != actual && !enc) {
+				if(actual.data.equals(elem)) {
+					enc = true;
+				}else {
+					actual = actual.next;
+				}
+			}
+		}
 		// COMPLETAR EL CODIGO Y CALCULAR EL COSTE
-		      
-		   }
+		return enc;
+	}
 
 	public T find(T elem) {
+		    if (contains(elem)) {
+		        Node<T> actual = this.first;
+		        do {
+		            if (actual.data.equals(elem)) {
+		                return actual.data; // Devuelve el elemento si se encuentra en la lista
+		            }
+		            actual = actual.next;
+		        } while (actual != this.first);
+		    }
+		    return null; // Devuelve null si el elemento no se encuentra en la lista		
 	//Determina si la lista contiene un elemento concreto, y develve su referencia, null en caso de que no est�
 		// COMPLETAR EL CODIGO Y CALCULAR EL COSTE
 
@@ -98,13 +165,27 @@ public class DoubleLinkedList<T> implements ListADT<T> {
 	public Iterator<T> iterator() { return new ListIterator(); } 
 
 	   // an iterator, doesn't implement remove() since it's optional 
-	   private class ListIterator implements Iterator<T> { 
+	private class ListIterator implements Iterator<T> {
+
+		private Node<T> actual = first;
+		   
+		@Override
+	    public boolean hasNext() {
+	        return actual != null; // Devuelve true si hay un siguiente elemento
+		}
+
+		@Override
+	    public T next() {
+	        if (!hasNext()) {
+	            throw new NoSuchElementException(); // Lanza una excepción si no hay más elementos
+	        }
+	        T data = actual.data;
+	        actual = actual.next; // Avanza al siguiente elemento
+	        return data;
+	    }
 
 		// COMPLETAR EL CODIGO Y CALCULAR EL COSTE
-
-
-
-	   } // private class
+	} // private class
 		
 		
          public void visualizarNodos() {
--- Entrega_2/alumnos/src/UnorderedDoubleLinkedList.java
+++ Entrega_2/alumnos/src/UnorderedDoubleLinkedList.java
@@ -3,20 +3,81 @@ package segundaFase;
 public class UnorderedDoubleLinkedList<T> extends DoubleLinkedList<T> implements UnorderedListADT<T> {
 	
 	public void addToFront(T elem) {
-	// añade un elemento al comienzo
+	// aï¿½ade un elemento al comienzo
 		// COMPLETAR EL CODIGO Y CALCULAR EL COSTE
-
+		if(elem == null) {
+			System.out.println("Introduce el elemento que quieres aÃ±adir");
+		}
+		else {
+			Node<T> nuevo = new Node<T>();
+			nuevo.descr = elem;
+			
+			if(this.first == null) {
+				nuevo.next= nuevo;
+				nuevo.prev=nuevo;
+				this.first=nuevo;
+			}
+			else {
+				nuevo.next=this.first;
+				nuevo.prev=this.first.prev;
+				this.first.prev=nuevo;
+				this.first.prev.next= nuevo;
+				this.first=nuevo;
+			}
+			this.count++;
+		}
 	}
 
 	public void addToRear(T elem) {
-	// añade un elemento al final 
+	// aï¿½ade un elemento al final 
 		// COMPLETAR EL CODIGO Y CALCULAR EL COSTE
-
+		if(elem == null) {
+			System.out.println("Introduce el elemento que quieres aÃ±adir");
+		}
+		else {
+			Node<T> nuevo = new Node<T>();
+			nuevo.descr = elem;
+			
+			if(this.first == null) {
+				nuevo.next= nuevo;
+				nuevo.prev=nuevo;
+				this.first=nuevo;
+			}
+			else {
+				this.first.prev.next=nuevo;
+				nuevo.prev=this.first.prev;
+				nuevo.next=this.first;
+				this.first.prev=nuevo;
+			}
+			this.count++;
 	}
+}
 	
 	public void addAfter(T elem, T target) {
-	// Añade elem detrás de otro elemento concreto, target,  que ya se encuentra en la lista
-		// ¡COMPLETAR OPCIONAL!
+	// Aï¿½ade elem detrï¿½s de otro elemento concreto, target,  que ya se encuentra en la lista
+		// ï¿½COMPLETAR OPCIONAL!
+		if(elem == null) {
+			System.out.println("Introduce el elemento que quieres aÃ±adir");
+		}else {
+			Node<T> actual = this.first.next;
+			while(actual != first) {
+				if(actual.next.descr == target) {
+					Node<T> nuevo = new Node<T>();
+					nuevo.descr = elem;
+					actual.next.prev=nuevo;
+					actual.next= nuevo;
+					nuevo.next= actual.next;
+					nuevo.prev=actual;
+					actual=nuevo;
+					this.count++;
+					return;
+					
+				}
+			actual=actual.next;
+			}
+			System.out.println("El elemento target no esta en la lista")
+		}
+		
 	}
 
 }
